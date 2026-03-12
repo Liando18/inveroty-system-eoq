148e91fb6e45f802bd22312124eea500631b70e4
macbook <liando1801@gmail.com>
2026-03-12 23:06:17 +0700

    Update PWA

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,21 +1,28 @@
-export default function manifest() {
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "4Yos Veterinary Care",
     short_name: "4YosApp",
     start_url: "/",
     display: "standalone",
+    orientation: "any",
     background_color: "#ffffff",
     theme_color: "#11df86",
+    lang: "en-US",
+    dir: "auto",
     icons: [
       {
-        src: "/icon192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icon512.png",
+        src: "/icon512_maskable.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/icon512_rounded.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
       },
     ],
   };
